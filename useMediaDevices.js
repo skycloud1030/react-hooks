251927123e4bcda849b6063878497cb2ad71e513
CT Hung <skycloud1030@gmail.com>
2020-01-08 15:12:42 +0800

    Fix useMediaDevices destroy bug

--- useMediaDevices.js
+++ useMediaDevices.js
@@ -1,28 +1,33 @@
 import { useEffect, useState } from "react";
 
 export default function useMediaDevices(video_ref, facingMode = "environment") {
-  const [stream, setStream] = useState();
   const [isReady, setReady] = useState(false);
   useEffect(() => {
+    let stream;
+    let isDirty = false;
     navigator.mediaDevices
       .getUserMedia({ video: { facingMode }, audio: false })
-      .then(stream => {
-        video_ref.current.srcObject = stream;
-        setStream(stream);
-        video_ref.current.oncanplay = () => {
-          video_ref.current.play();
-          video_ref.current.width = video_ref.current.videoWidth;
-          video_ref.current.height = video_ref.current.videoHeight;
-          setReady(true);
-        };
+      .then(media => {
+        if(!isDirty){
+          stream = media;
+          video_ref.current.srcObject = media;
+          video_ref.current.oncanplay = () => {
+            video_ref.current.width = video_ref.current.videoWidth;
+            video_ref.current.height = video_ref.current.videoHeight;
+            video_ref.current.play();
+            setReady(true);
+          };
+        }
       });
-
     return () => {
       // release stream
       setReady(false);
-      stream.getTracks().forEach(track => {
-        track.stop();
-      });
+      isDirty = true;
+      if(stream){
+        stream.getTracks().forEach(track => {
+          track.stop();
+        });
+      }
     };
   }, [facingMode]);
 
